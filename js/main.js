53be3144d9ed6b61c7c9deaa04cc104a3e5b6c57
welbym <welbymoore0816@gmail.com>
2020-02-19 10:28:53 -0600

    Update main.js
    Switched month and day in getDateTime()

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function getDateTime() {
         hour = 24 + hour;
     }
 
-    let date = (day < 10 ? '0' + day : day) + '/' + (month < 10 ? '0' + month : month) + '/' + dateTime.getFullYear();
+    let date = (month < 10 ? '0' + month : month) + '/' + (day < 10 ? '0' + day : day) + '/' + dateTime.getFullYear();
     let time = (hour < 10 ? '0' + hour : hour) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
 
     return date + '\n' + time;
